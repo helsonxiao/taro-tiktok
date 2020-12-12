875349963389763506d0298858f23ffea231ef7a
helsonxiao <helsonxiao@gmail.com>
2020-12-12 14:41:51 +0800

    feat: 自动格式化代码风格

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-  'extends': ['taro/react']
+  extends: ['taro/react', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'no-duplicate-imports': 'error',
+    'no-alert': 'warn',
+    'no-console': 'warn',
+  },
 }
